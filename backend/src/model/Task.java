65230d0953e63e075097824ea05636557f5d49c9
Lemon House <2804622326@qq.com>
2025-07-11 11:19:11 +0100

    Add backend skeleton

---
+++ backend/src/model/Task.java
@@ -0,0 +1,47 @@
+package com.wattwise.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private boolean completed;
+
+    public Task() {}
+
+    public Task(String name, boolean completed) {
+        this.name = name;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
